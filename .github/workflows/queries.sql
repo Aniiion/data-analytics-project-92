ac7a6c8e0731f836f963e57aca3bf34255ecb7cf
Aniiion <167976508+Aniiion@users.noreply.github.com>
2024-11-21 17:57:59 +0300

    queries.sql

--- .github/workflows/queries.sql
+++ .github/workflows/queries.sql
@@ -47,3 +47,68 @@ on tab.sales_person_id = emp.employee_id
 group by seller, tab.income, tab.operations
 order by income desc
 limit 10;
+
+-- Запрос day_of_the_week_income.csv
+SELECT 
+    CONCAT(emp.first_name, ' ', emp.last_name) AS seller,
+    TO_CHAR(s.sale_date, 'Day') AS day_of_week,
+    ROUND(SUM(s.quantity * p.price)) AS income
+FROM 
+    sales s
+INNER JOIN 
+    products p ON s.product_id = p.product_id
+INNER JOIN 
+    employees emp ON s.sales_person_id = emp.employee_id
+GROUP BY 
+    emp.first_name, emp.last_name, TO_CHAR(s.sale_date, 'Day'), EXTRACT(DOW FROM s.sale_date)
+ORDER BY 
+    EXTRACT(DOW FROM s.sale_date), seller;
+-- Запрос для age_groups.csv
+SELECT 
+    CASE 
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        ELSE '40+' 
+    END AS age_category,
+    COUNT(*) AS count
+FROM 
+    customers
+GROUP BY 
+    age_category
+ORDER BY 
+    MIN(age) ASC;
+-- Запрос для customers_by_month.csv 
+SELECT 
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS date,
+    COUNT(DISTINCT c.customer_id) AS total_customers,
+    SUM(s.quantity * p.price) AS income
+FROM 
+    sales s
+JOIN 
+    products p ON s.product_id = p.product_id
+JOIN 
+    customers c ON s.customer_id = c.customer_id
+GROUP BY 
+    date
+ORDER BY 
+    date ASC;
+-- Запрос для special_offer.csv 
+SELECT DISTINCT
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    s.sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM
+    customers c
+JOIN sales s ON c.customer_id = s.customer_id
+JOIN products p ON s.product_id = p.product_id
+JOIN employees e ON s.sales_person_id = e.employee_id
+WHERE
+    s.sale_date = (
+        SELECT MIN(s1.sale_date)
+        FROM sales s1
+        WHERE s1.customer_id = c.customer_id
+    )
+    AND p.price = 0
+ORDER BY
+    customer, seller;
+
